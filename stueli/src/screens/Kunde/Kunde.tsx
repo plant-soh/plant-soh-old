7ffcd7a829b2334ad719ad114326c43d0a6cc7d5
Martin Mueller <damadden88@googlemail.com>
2023-01-03 18:44:42 +0100

    feature: Admin kann neue Kunden eingeben

--- stueli/src/screens/Kunde/Kunde.tsx
+++ stueli/src/screens/Kunde/Kunde.tsx
@@ -1,16 +1,33 @@
-import { useListAnlagesQuery } from '../../lib/react-api';
+import { useState } from 'react';
+import {
+  useCreateAnlageMutation,
+  useDeleteAnlageMutation,
+  useListAnlagesQuery,
+} from '../../lib/react-api';
 
 const columns: { [key: string]: string } = {
   firma: 'Firma',
   standort: 'Standort',
   anschrift: 'Anchrift',
-  // bemerkung: 'Bemerkung',
+  actions: 'Aktionen',
 };
 
 const Kunde = () => {
-  const { data, isLoading } = useListAnlagesQuery(undefined, {
+  const { data, isLoading, refetch } = useListAnlagesQuery(undefined, {
     refetchOnWindowFocus: false,
   });
+
+  const deleteAnlage = useDeleteAnlageMutation();
+  const createAnlage = useCreateAnlageMutation();
+
+  const [newAnlage, setNewAnlage] = useState<
+    | {
+        firma?: string;
+        standort?: string;
+        anschrift?: string;
+      }
+    | undefined
+  >(undefined);
 
   if (isLoading) return <div>Loading...</div>;
 
@@ -35,19 +52,113 @@ const Kunde = () => {
           </tr>
         </thead>
         <tbody className="bg-gray-50" data-testid="recent-calls-table-body">
-          {data?.listAnlages?.items?.map((anlage, row_index) => (
-            <tr key={row_index} className="border-b border-gray-400">
-              {Object.keys(columns).map((col, index) => (
-                <td key={index} className="p-3 text-left whitespace-pre-line">
-                  {col === 'firma'
-                    ? anlage?.firma
-                    : col === 'standort'
-                    ? anlage?.standort
-                    : anlage?.anschrift}
-                </td>
-              ))}
-            </tr>
-          ))}
+          {data?.listAnlages?.items?.map(
+            (anlage, row_index) =>
+              anlage && (
+                <tr key={row_index} className="border-b border-gray-400">
+                  {Object.keys(columns)
+                    .slice(0, -1)
+                    .map((col, index) => (
+                      <td
+                        key={index}
+                        className="p-3 text-left whitespace-pre-line"
+                      >
+                        {col === 'firma'
+                          ? anlage?.firma
+                          : col === 'standort'
+                          ? anlage?.standort
+                          : anlage?.anschrift}
+                      </td>
+                    ))}
+                  <td
+                    key="action"
+                    className="p-3 text-left whitespace-pre-line"
+                  >
+                    <button
+                      className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+                      onClick={async () => {
+                        await deleteAnlage.mutateAsync({
+                          input: {
+                            id: anlage?.id,
+                          },
+                        });
+                        await refetch();
+                      }}
+                    >
+                      <span>Löschen</span>
+                    </button>
+                  </td>
+                </tr>
+              ),
+          )}
+          <tr key="insert_row" className="border-b border-gray-400">
+            <td
+              key="firma_insert"
+              className="p-3 text-left whitespace-pre-line"
+            >
+              <input
+                type="text"
+                id="firma"
+                name="firma"
+                onChange={(e) =>
+                  setNewAnlage({ ...newAnlage, firma: e.target.value })
+                }
+              />
+            </td>
+            <td
+              key="standort_insert"
+              className="p-3 text-left whitespace-pre-line"
+            >
+              <input
+                type="text"
+                id="standort"
+                name="standort"
+                onChange={(e) =>
+                  setNewAnlage({ ...newAnlage, standort: e.target.value })
+                }
+              />
+            </td>
+            <td
+              key="anschrift_insert"
+              className="p-3 text-left whitespace-pre-line"
+            >
+              <input
+                type="text"
+                id="anschrift"
+                name="firma"
+                onChange={(e) =>
+                  setNewAnlage({ ...newAnlage, anschrift: e.target.value })
+                }
+              />
+            </td>
+            <td key="action" className="p-3 text-left whitespace-pre-line">
+              {!newAnlage?.firma || !newAnlage?.standort ? (
+                <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded opacity-50 cursor-not-allowed">
+                  <span>Einfügen</span>
+                </button>
+              ) : (
+                <button
+                  className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+                  onClick={async () => {
+                    if (!newAnlage?.firma || !newAnlage?.standort) {
+                      console.debug('newAnlage missing');
+                      return;
+                    }
+                    await createAnlage.mutateAsync({
+                      input: {
+                        firma: newAnlage.firma,
+                        standort: newAnlage.standort,
+                        anschrift: newAnlage.anschrift,
+                      },
+                    });
+                    await refetch();
+                  }}
+                >
+                  <span>Einfügen</span>
+                </button>
+              )}
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
