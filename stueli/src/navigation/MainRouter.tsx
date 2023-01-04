0c73c88a34fd5ead33e23b73dff8ed710fe71472
Martin Mueller <damadden88@googlemail.com>
2023-01-04 16:33:41 +0100

    feature: use lazy loading

--- stueli/src/navigation/MainRouter.tsx
+++ stueli/src/navigation/MainRouter.tsx
@@ -1,25 +1,37 @@
 /* istanbul ignore file */
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Role } from '../lib/api';
+
 import { useAuth } from '../providers/AuthProvider';
 
-import Home from '../screens/Home';
-import Kunde from '../screens/Kunde';
-import KundeUser from '../screens/KundeUser';
-import ReferenzStueli from '../screens/ReferenzStueckliste';
-import NavigationLayout from './NavigationLayout';
+const Home = lazy(() => import('../screens/Home'));
+const Kunde = lazy(() => import('../screens/Kunde'));
+const KundeUser = lazy(() => import('../screens/KundeUser'));
+const ReferenzStueckliste = lazy(
+  () => import('../screens/ReferenzStueckliste'),
+);
+const NavigationLayout = lazy(() => import('../navigation/NavigationLayout'));
+
+const withSuspense = (Component: any) => {
+  return (
+    <Suspense fallback={<div></div>}>
+      <Component />
+    </Suspense>
+  );
+};
 
 const MainRouter = () => {
   const { role } = useAuth();
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<NavigationLayout />}>
-          <Route index element={<Home />} />
-          <Route path="kunde" element={<Kunde />} />
-          <Route path="kunde/:id" element={<ReferenzStueli />} />
+        <Route path="/" element={withSuspense(NavigationLayout)}>
+          <Route index element={withSuspense(Home)} />
+          <Route path="kunde" element={withSuspense(Kunde)} />
+          <Route path="kunde/:id" element={withSuspense(ReferenzStueckliste)} />
           {role === Role.Admin && (
-            <Route path="/kundeuser" element={<KundeUser />} />
+            <Route path="/kundeuser" element={withSuspense(KundeUser)} />
           )}
         </Route>
         <Route path="*" element={<div>Error</div>} />
